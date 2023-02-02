e5175bccd3d74a47427a7cc9b6ca3a9efaa6cc9f
TuvaStange <tuvsta13@o365.mrvgs.no>
2023-02-02 14:36:23 +0100

    ja

--- redigerings-blokk/src/app/app.module.ts
+++ redigerings-blokk/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './components/view/view.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
