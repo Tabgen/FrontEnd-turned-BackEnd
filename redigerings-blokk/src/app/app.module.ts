d7c2a6360274e5e29ace55f742660695640737a5
TuvaStange <tuvsta13@o365.mrvgs.no>
2023-02-03 08:55:30 +0100

    uhhhuu

--- redigerings-blokk/src/app/app.module.ts
+++ redigerings-blokk/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewComponent } from './components/view/view.component';
 import { EditComponent } from './components/edit/edit.component';
+import { InfoComponent } from './component/info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewComponent,
-    EditComponent
+    EditComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ redigerings-blokk/src/app/mock-info.ts
@@ -0,0 +1,9 @@
+import { Info } from './Info'
+
+export const INFO: Info[] = [
+    {
+        id: 1,
+        title: 'Measures to support accessibility',
+        text: 'Visma Enterprise AS takes the following measures to ensure accessibility of Visma Flyt Ressursstyring:',
+    }
+]
