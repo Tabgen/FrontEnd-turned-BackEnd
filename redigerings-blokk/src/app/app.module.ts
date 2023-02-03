a9ac102db41fb60cb4368b1db6adbb8a7e68ae60
TuvaStange <tuvsta13@o365.mrvgs.no>
2023-02-03 09:12:13 +0100

    fix!

--- redigerings-blokk/src/app/app.module.ts
+++ redigerings-blokk/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewComponent } from './components/view/view.component';
 import { EditComponent } from './components/edit/edit.component';
-import { InfoComponent } from './component/info/info.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
